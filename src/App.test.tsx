660232f73e23c11c26fe1b310cd3292d3c5f54c5
Vitalii Molnar <vitaliy.molnar@gmail.com>
2020-11-11 17:24:30 +0000

    Revert "some changes"
    This reverts commit 6fd95d2508240bb14e7852b38f16f7967a55b0d0

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,9 @@
-const puppeteer = require('puppeteer')
-const assert = require('assert')
-let browser
-let page
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-
-
-  test('has title "YouTube"', async () => {
-    try {
-      const browser = await puppeteer.launch({ headless: false });
-      const page = await browser.newPage();
-      await page.goto('http://localhost:3000', {waitUntil: 'domcontentloaded'});
-      
-      await new Promise(resolve =>  setTimeout(resolve, 30000));
-      console.log('done');
-  } catch (e) {
-      console.log('Err', e);
-  }
-  jest.setTimeout(30000);
-  })
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
