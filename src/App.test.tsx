3d32c6ee0c852fa1cf67fd845b3bde78a1420c4f
Vitalii Molnar <vitaliy.molnar@gmail.com>
2020-11-13 12:18:57 +0200

    added test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+const assert = require('assert'),
+puppeteer = require('puppeteer');
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let browser, page;
+
+beforeEach(async()=>{
+  browser = await puppeteer.launch({
+    headless: true
+  });
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/Performance");
 });
+afterEach(async()=>{
+  await browser.close();
+})
+
+test("test home page url", async()=>{
+  const url  = await page.url();
+  assert(url==="http://localhost:3000/Performance");
+})
