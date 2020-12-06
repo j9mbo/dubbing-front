f162fec17a6f457fdb6516033157f5bc2752461c
Vitalii Molnar <vitaliy.molnar@gmail.com>
2020-12-06 23:04:28 +0200

    adding backend

--- src/util/api.tsx
+++ src/util/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://ec2-18-134-245-19.eu-west-2.compute.amazonaws.com:5000/`,
+  baseURL: `http://ec2-35-178-204-2.eu-west-2.compute.amazonaws.com:5000/`,
 });
