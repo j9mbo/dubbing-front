920923dcf6e25ebf77a65631cdfe7eb1398d4619
Vitalii Molnar <vitaliy.molnar@gmail.com>
2020-12-06 22:03:25 +0200

    adding backend

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -116,7 +116,7 @@ module.exports = function(webpackEnv) {
     
     externals: {
       'Config': JSON.stringify({
-        serverUrl: "http://ec2-18-130-235-217.eu-west-2.compute.amazonaws.com:5000/"
+        serverUrl: "http://ec2-18-134-245-19.eu-west-2.compute.amazonaws.com:5000/"
       })
     },
     mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
--- src/util/api.tsx
+++ src/util/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://ec2-18-130-235-217.eu-west-2.compute.amazonaws.com:5000/api/`,
+  baseURL: `http://ec2-18-134-245-19.eu-west-2.compute.amazonaws.com:5000/api/`,
 });
